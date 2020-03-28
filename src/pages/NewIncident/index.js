860e183d1800351ae816b8885e1a841902443bf6
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-03-28 20:15:12 -0300

    create new incident page

--- src/pages/NewIncident/index.js
+++ src/pages/NewIncident/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+import logoImg from '../../assets/logo.svg'
+import { FiArrowLeft } from 'react-icons/fi'
 
 import './style.css'
 
 export default function NewIncident() {
   return (
-    <div />
+    <div className="new-incident-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be the Hero"/>
+
+          <h1>Cadastrar novo caso</h1>
+
+          <p>Descreva o caso detalhadamente para encontrar um heroi para resolver isso.</p>
+
+          <Link className="back-link" to="/profile">
+            <FiArrowLeft size={20} color="#E02041" />
+            Voltar para Home
+          </Link>
+        </section>
+
+        <form>
+          <input placeholder="Título do caso"/>
+          <textarea placeholder="Descrição" />
+          <input placeholder="Valor em reais" />
+
+          <button className="button" type="submit">Cadastrar</button>
+        </form>
+      </div>
+    </div>
   );
 }
